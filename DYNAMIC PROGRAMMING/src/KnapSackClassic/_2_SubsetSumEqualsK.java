01d2142dd43bbc8892bb4c4d8a4bc103106262c9
Anurag Jaiswal <anuragj0806@gmail.com>
2024-02-23 18:53:59 +0530

    Explanations added

--- DYNAMIC PROGRAMMING/src/KnapSackClassic/_2_SubsetSumEqualsK.java
+++ DYNAMIC PROGRAMMING/src/KnapSackClassic/_2_SubsetSumEqualsK.java
@@ -5,50 +5,50 @@ package KnapSackClassic;
 
 public class _2_SubsetSumEqualsK {
 
-        // MEMOIZATION :   time : O(N*sum)
-        public static boolean memoization(int[] arr, int n, int sum, int idx, Boolean[][] memo) {
-            if(sum == 0) return true;  // THIS BASE CASE SHOULD COME FIRST before other base cases
-            
-            if(idx==n || sum < 0) return false;
-            if(memo[idx][sum] != null) return memo[idx][sum];
-            
-            // include and don't include
-            boolean included = memoization(arr, n, sum-arr[idx], idx+1, memo);
-            boolean excluded = memoization(arr, n, sum, idx+1, memo);
-            
-            memo[idx][sum] = included || excluded;
-            return memo[idx][sum];
+    // MEMOIZATION :   time : O(N*sum)
+    public static boolean memoization(int[] arr, int n, int sum, int idx, Boolean[][] memo) {
+        if(sum == 0) return true;  // THIS BASE CASE SHOULD COME FIRST before other base cases
+        
+        if(idx==n || sum < 0) return false;
+        if(memo[idx][sum] != null) return memo[idx][sum];
+        
+        // include and don't include
+        boolean included = memoization(arr, n, sum-arr[idx], idx+1, memo);
+        boolean excluded = memoization(arr, n, sum, idx+1, memo);
+        
+        memo[idx][sum] = included || excluded;
+        return memo[idx][sum];
+    }
+
+
+    // TABULATION
+    public static boolean tabulation(int[] arr, int target) {
+        int n = arr.length;
+        boolean[][] dp = new boolean[n+1][target+1];
+        // base case initialization
+        for(int i = 0; i < n+1; i++) {  // 0th col bcz we will always have a subset whose sum is zero and that is empty subset
+            dp[i][0] = true;
         }
-
-
-        // TABULATION
-        public static boolean tabulation(int[] arr, int target) {
-            int n = arr.length;
-            boolean[][] dp = new boolean[n+1][target+1];
-            // base case initialization
-            for(int i = 0; i < n+1; i++) {  // 0th col bcz we will always have a subset whose sum is zero and that is empty subset
-                dp[i][0] = true;
-            }
-            // NOTE : Here we have to initialize 0th row to false also bcz if we do not have
-            // any item to add so obviously we can't make it to the target sum
-            // but in java already false values are present so not doing that
-            // but dp[0][0] would be true from the 1st col 0th row start filling F
-            // i items and j is target sum
-            for(int i = 1; i < n+1; i++) {
-                for(int j = 1; j < target+1; j++) {
-                    if(arr[i-1] <= j && dp[i-1][j-arr[i-1]] == true) {  // if including it gives true update the dp array to true otherwise its already false
-                        dp[i][j] = true;
-                    } else if(dp[i-1][j] == true) {
-                        dp[i][j] = true;
-                    }
+        // NOTE : Here we have to initialize 0th row to false also bcz if we do not have
+        // any item to add so obviously we can't make it to the target sum
+        // but in java already false values are present so not doing that
+        // but dp[0][0] would be true from the 1st col 0th row start filling F
+        // i items and j is target sum
+        for(int i = 1; i < n+1; i++) {
+            for(int j = 1; j < target+1; j++) {
+                if(arr[i-1] <= j && dp[i-1][j-arr[i-1]] == true) {  // if including it gives true update the dp array to true otherwise its already false
+                    dp[i][j] = true;
+                } else if(dp[i-1][j] == true) {
+                    dp[i][j] = true;
                 }
             }
-            //you can do this to include & don't include way using || or
-            // eg: dp[i][j] = dp[i-1][dp[j-arr[i-1]]] || dp[i-1][j];   include || exclude
-            // but above one is lil bit optimised as we are leveraging the already filled false value
-            print(dp);
-            return dp[n][target];
         }
+        //you can do this to include & don't include way using || or
+        // eg: dp[i][j] = dp[i-1][dp[j-arr[i-1]]] || dp[i-1][j];   include || exclude
+        // but above one is lil bit optimised as we are leveraging the already filled false value
+        print(dp);
+        return dp[n][target];
+    }
 
     public static void main(String[] args) {
         int[] arr = {4,2,7,1,3};
--- DYNAMIC PROGRAMMING/src/KnapSackClassic/_7_MinimumSumPartition.java
+++ DYNAMIC PROGRAMMING/src/KnapSackClassic/_7_MinimumSumPartition.java
@@ -1,14 +1,31 @@
 /*
 https://www.geeksforgeeks.org/problems/minimum-sum-partition3317/1?
 
+Question :
+Given an array arr of size n containing non-negative integers, the task is to divide
+it into two sets S1 and S2 such that the absolute difference between their sums is 
+minimum and find the minimum difference.
+Input: N = 4, arr[] = {1, 6, 11, 5} 
+Output: 1
+Explanation: 
+Subset1 = {1, 5, 6}, sum of Subset1 = 12 
+Subset2 = {11}, sum of Subset2 = 11
+
+
+EXPLANATION WITH IMAGE IS GIVEN IN THIS PACKAGE _7_MinimumSumPartition.jpg ⭐⭐⭐
 */
 
 package KnapSackClassic;
 
+import java.util.Arrays;
+
 public class _7_MinimumSumPartition {
-    public static int tabulation(int[] arr, int target) {
-        int n = arr.length;
-        boolean[][] dp = new boolean[n+1][target+1];
+
+    // TABULATION
+    public static int tabulation(int[] arr, int n) {
+        int total = Arrays.stream(arr).sum();
+
+        boolean[][] dp = new boolean[n+1][(total/2)+1];  // total/2
         // initialization
         for(int i = 0; i < n+1; i++) {  // 0th col
             dp[i][0] = true;
@@ -19,7 +36,7 @@ public class _7_MinimumSumPartition {
         // but dp[0][0] would be true from the 1st col 0th row start filling F
         // i items and j is target sum
         for(int i = 1; i < n+1; i++) {
-            for(int j = 1; j < target+1; j++) {
+            for(int j = 1; j < dp[0].length; j++) {
                 if(arr[i-1] <= j && dp[i-1][j-arr[i-1]] == true) {  // if including it gives true update the dp array to true otherwise its already false
                     dp[i][j] = true;
                 } else if(dp[i-1][j] == true) {
@@ -27,19 +44,48 @@ public class _7_MinimumSumPartition {
                 }
             }
         }
-
+        print(dp);
         int minDiff = Integer.MAX_VALUE;
-        for(int i = 0; i <= (target/2); i++) {    // target/2 = (total/2)
+        for(int i = 0; i <= (total/2); i++) {    // target/2 = (total/2)
             if(dp[n][i]) {  // possible subsets
-               minDiff = Math.min(target - (2*i), minDiff);
+                int s1 = i;         // subset sum1
+                int s2 = total-i;   //subset sum2
+               minDiff = Math.min(Math.abs(s1-s2), minDiff);
             }
         }
         return minDiff;
     }
 
-    public int minDifference(int arr[], int n) { 
-        int range = 0;
-        for(int i = 0; i < n;i++) range += arr[i];
-        return tabulation(arr, range);
+    public static void main(String[] args) { 
+        int[] arr = {1,6,11,5};
+        int n = arr.length;
+        System.out.println(tabulation(arr, n));
+
+        // NOTE: ⭐⭐
+        // PERFORM THIS USING TABULATION ONLY BECAUSE IT IS MORE INTUITIVE WITH
+        // TABULATION APPRAOCH + IF YOU USE MEMOIZATION IT WILL GIVE WRONG RESULTS
+        // BECUASE LOOK TABLE IS FILLED DIFFERENTLY IN MEMOIZATION.
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    static void print(boolean[][] dp) {
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
